3f70d544f797637915698638894006b4538872f8
juit <pulkit.cloud@gmail.com>
2015-11-26 02:56:13 +0530

    SPA with PHP Live

---
+++ nav.php
@@ -0,0 +1,20 @@
+
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link href="menu.css" rel="stylesheet" type="text/css" />
+<ul class="drawer">
+  <li> <a href="home.php"> <i class="fa fa-info-circle"></i> <span>Home</span> </a>
+    <ul>
+      <li> <a href="home.php"> <i class="fa fa-question-circle"></i> <span>About</span> </a> </li>
+      <li> <a href="home.php"> <i class="fa fa-phone-square"></i> <span>Contact</span> </a> </li>
+    </ul>
+  </li>
+  
+  <li> <a href="about.php"> <i class="fa fa-share-alt"></i> <span>Social</span> </a>
+    <ul>
+      <li> <a href="about.php"> <i class="fa fa-facebook"></i> <span>Facebook</span> </a> </li>
+      <li> <a href="about.php"> <i class="fa fa-twitter"></i> <span>Twitter</span> </a> </li>
+      <li> <a href="about.php"> <i class="fa fa-google-plus"></i> <span>Google+</span> </a> </li>
+      <li> <a href="about.php"> <i class="fa fa-pinterest"></i> <span>Pinterest</span> </a> </li>
+    </ul>
+  </li>
+</ul>
